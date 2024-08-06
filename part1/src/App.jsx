9e8f861dbadaae631e3731c83f79d4d73bff2811
osgofre <osgofre13@gmail.com>
2024-08-06 18:31:37 +0200

    Fix unicafe

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,15 +4,22 @@ import { useState } from 'react'
 const StatisticLine = ({text, value}) => <p>{text} {value}</p>
 
 
-const Statistics = ({ good, neutral, bad, all, average, positive, handleClickGood, handleClickNeutral, handleClickBad }) => {
+const Statistics = ({ good, neutral, bad}) => {
+  const all = good+neutral+bad
+  const average = all != 0 ? (good-bad)/all : 0
+  const positive = all != 0 ? (good)/all * 100 : 0
+
+  if (all === 0) {
+    return (
+      <div className='statistics'>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
   return (
     <div className='statistics'>
-      <h1>give feedback</h1>
-
-      <button onClick={handleClickGood}>good</button>
-      <button onClick={handleClickNeutral}>neutral</button>
-      <button onClick={handleClickBad}>bad</button>
-
       <h1>statistics</h1>
       <StatisticLine text="good" value={good} />
       <StatisticLine text="neutral" value={neutral} />
@@ -31,27 +38,22 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const all = good+neutral+bad
-  const average = all != 0 ? (good-bad)/all : 0
-  const positive = all != 0 ? (good)/all * 100 : 0
-
   const handleClickGood = () => setGood(good+1)
   const handleClickNeutral = () => setNeutral(neutral+1)
   const handleClickBad = () => setBad(bad+1)
 
-
   return (
     <div>
+      <h1>give feedback</h1>
+
+      <button onClick={handleClickGood}>good</button>
+      <button onClick={handleClickNeutral}>neutral</button>
+      <button onClick={handleClickBad}>bad</button>
+
       <Statistics 
         good={good}
         neutral={neutral}
         bad={bad}
-        all={all}
-        average={average}
-        positive={positive}
-        handleClickGood={handleClickGood}
-        handleClickNeutral={handleClickNeutral}
-        handleClickBad={handleClickBad}
       />
     </div>
   )
