15cb3ebbab6db1defa90675d49c6cf5c3c66d6d7
osgofre <osgofre13@gmail.com>
2024-08-06 18:25:42 +0200

    Part 1 - unicafe

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,31 +1,58 @@
-import { Content } from "./Components/Content"
-import { Header } from "./Components/Header"
-import { Total } from "./Components/Total"
+import { useState } from 'react'
+
+
+const StatisticLine = ({text, value}) => <p>{text} {value}</p>
+
+
+const Statistics = ({ good, neutral, bad, all, average, positive, handleClickGood, handleClickNeutral, handleClickBad }) => {
+  return (
+    <div className='statistics'>
+      <h1>give feedback</h1>
+
+      <button onClick={handleClickGood}>good</button>
+      <button onClick={handleClickNeutral}>neutral</button>
+      <button onClick={handleClickBad}>bad</button>
+
+      <h1>statistics</h1>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={`${positive} %`} />
+    </div>
+  )
+
+}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const all = good+neutral+bad
+  const average = all != 0 ? (good-bad)/all : 0
+  const positive = all != 0 ? (good)/all * 100 : 0
+
+  const handleClickGood = () => setGood(good+1)
+  const handleClickNeutral = () => setNeutral(neutral+1)
+  const handleClickBad = () => setBad(bad+1)
+
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total parts={course.parts} />
+      <Statistics 
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+        positive={positive}
+        handleClickGood={handleClickGood}
+        handleClickNeutral={handleClickNeutral}
+        handleClickBad={handleClickBad}
+      />
     </div>
   )
 }
