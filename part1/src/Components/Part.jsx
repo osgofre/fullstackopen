a4610e97bb7b41ded627920ce438c1d58a802599
osgofre <osgofre13@gmail.com>
2024-08-06 18:04:43 +0200

    Part 1 - courseinfo

---
+++ part1/src/Components/Part.jsx
@@ -0,0 +1,1 @@
+export const Part = ({part, exercise}) => <p>{part} {exercise}</p>
---
+++ part1/src/Components/Total.jsx
@@ -0,0 +1,4 @@
+export const Total = ({parts}) => {
+    const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+    return <p><strong>Number of exercises {total}</strong></p>
+}
